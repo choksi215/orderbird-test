2d6c76bab64a5fd175e98caa4c2f9665c340bb7e
choksi215 <choksi215@gmail.com>
2023-07-31 12:16:57 +0200

    Error handling and commenting the code

--- src/js/main.js
+++ src/js/main.js
@@ -8,32 +8,34 @@ function calculatePopularity(stars, forks){
 function displaySearchResults(data) {
     const searchResultsDiv = document.getElementById('searchResults');
 
-    // You can perform a search using the query and display the results here
-    // For this example, we'll just display the query as the result
+    // We perform a search using the query and display the results here
     searchResultsDiv.innerHTML = `
     <div class="flex justify-center mt-6 md:px-32 py-8 w-full">
         <div class="shadow overflow-hidden rounded">
             <div class="bg-[#decff1] rounded-lg">
-                <div class="flex flex-col space-y-3 p-5 text-gray-700">
+                <div class="flex flex-col space-y-3 p-5 text-gray-700 min-w-[340px]">
                     <div class="flex justify-between">
-                        <b class="w-1/3 text-left py-3 px-4">Repository Name: </b>
-                        <span class="w-1/3 text-left py-3 px-4">${data.rName}</span>
+                        <img class="shadow-xl rounded-full align-middle border-none max-w-[150px] my-4 mx-auto" src="${data.rAvatarUrl}">
                     </div>
                     <div class="flex justify-between">
-                        <b class="w-1/3 text-left py-3 px-4">Repository Autor:</b>
-                        <span class="w-1/3 text-left py-3 px-4">${data.rAutor}</span>
+                        <b class="w-1/2 text-left py-3 px-4">Repository Name: </b>
+                        <span class="w-1/2 text-right py-3 px-4">${data.rName}</span>
                     </div>
                     <div class="flex justify-between">
-                        <b class="w-1/3 text-left py-3 px-4">Stars:</b>
-                        <span class="w-1/3 text-left py-3 px-4">${data.rStars}</span>
+                        <b class="w-1/2 text-left py-3 px-4">Repository Autor:</b>
+                        <span class="w-1/2 text-right py-3 px-4">${data.rAutor}</span>
                     </div>
                     <div class="flex justify-between">
-                        <b class="w-1/3 text-left py-3 px-4">Forks:</b>
-                        <span class="w-1/3 text-left py-3 px-4">${data.rForks}</span>
+                        <b class="w-1/2 text-left py-3 px-4">Stars:</b>
+                        <span class="w-1/2 text-right py-3 px-4">${data.rStars}</span>
                     </div>
                     <div class="flex justify-between">
-                        <b class="w-1/3 text-left py-3 px-4">Popularity:</b>
-                        <span class="w-1/3 text-left py-3 px-4">${data.rPopularity}</span>
+                        <b class="w-1/2 text-left py-3 px-4">Forks:</b>
+                        <span class="w-1/2 text-right py-3 px-4">${data.rForks}</span>
+                    </div>
+                    <div class="flex justify-between">
+                        <b class="w-1/2 text-left py-3 px-4">Popularity:</b>
+                        <span class="w-1/2 text-right py-3 px-4">${data.rPopularity ? "Popular &#127881" : "Non popular &#128078"}</span>
                     </div>
                 </div>
             </div>
@@ -43,6 +45,7 @@ function displaySearchResults(data) {
 }
 
 function repoListDTO(rList) {
+    // We scrape the data from the initial object here
     const repo = rList.data.items[0];
 
     const rName = repo.name;
@@ -52,7 +55,7 @@ function repoListDTO(rList) {
     const rForks = repo.forks;
 
     const rPopularity = calculatePopularity(rStars, rForks);
-
+    // and pass it to the display function
     displaySearchResults({
         rName,
         rAutor,
@@ -64,6 +67,7 @@ function repoListDTO(rList) {
 }
 
 async function fetchRepoData(name) {
+    // Make the API request
     const octokit = new Octokit({ auth: process.env.AUTH_GITHUB_KEY });
 
     const repoList = await octokit.request(`GET /search/repositories?q=${name}`, {
@@ -72,26 +76,40 @@ async function fetchRepoData(name) {
         }
     })
 
-    console.log(repoList)
+    // handle bed response
+    if (!repoList.status === 200) {
+        throw new Error('bad response');
+    }
 
-    // repoList va poi passato ad una funzione di retrieve dei dati, che poi verranno passati ad una funzione di visualization.
-    repoListDTO(repoList)
+    // Handle empty obj
+    if (repoList.data.items.length) {
+        // we pass the response object to the DTO (data transfer object) function for scraping
+        repoListDTO(repoList)
+    } else {
+        // we throw an error
+        throw new Error('string invalid');
+    }
 }
 
 
 function handleSubmit(event) {
     event.preventDefault(); 
     const searchQuery = document.getElementById('searchInput').value;
-    if(searchQuery==="") return 
-    fetchRepoData(searchQuery)
+    // to exit when given an empty string
+    if(searchQuery==="") return
+
+    try {
+        fetchRepoData(searchQuery)
+    } catch(err) {
+        console.log(err)
+    }
 }
 
 function submitListener() {
+    // submit listener
     document.getElementById('searchForm').addEventListener('submit', handleSubmit);
 }
 
 export function main() {
     submitListener();
-    // const repoData = repoNameRequest(repoName);
-    // appendRepoData(repoData);
 }
